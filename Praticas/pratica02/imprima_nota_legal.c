d87b153c7c25d0961053685215f738d2e3ef015e
Gustavo <gustiftw21@gmail.com>
2025-09-03 10:47:04 -0300

    Adicionando arquivos da pratica02

--- Praticas/pratica02/imprima_nota_legal.c
+++ Praticas/pratica02/imprima_nota_legal.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main() {
-printf ("%s\n","==============================");
-printf ("%s\n","Nota   Legal");
+int main()
+{
+    printf("==============================\n");
+    printf("     N O T A    L E G A L\n");
+    printf("==============================\n");
+    printf("Produto      Qtd Valor Unit\n");
+    printf("%-12s %03i %13.2f\n", "Camiseta" , 002 , 39.99);
+    printf("%-12s %03i %13.2f\n", "Calça" , 001 , 89.90);
+    printf("%-12s %03i %13.2f\n", "Meia Social" , 003 , 19.99);
+    printf("==============================\n");
+    printf("Total:                  229.85\n");
     return 0;
 }
