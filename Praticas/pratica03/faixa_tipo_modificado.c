340725791caacbb4706a5c142fefb2a0a6b40a0c
Gustavo <gustiftw21@gmail.com>
2025-09-12 16:28:07 -0300

    adicionando arquivos da pratica03

---
+++ Praticas/pratica03/faixa_tipo_modificado.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(){
+    printf("O tipo 'unsigned char' aceita valores entre %i e %i.\n", 0, UCHAR_MAX);
+    printf("O tipo 'short int' aceita valores entre %i e %i.\n", SHRT_MIN, SHRT_MAX);
+    printf("O tipo 'unsigned short int' aceita valores entre %i e %i.\n", 0, USHRT_MAX);
+    printf("O tipo 'long int' aceita valores entre %li e %li.\n", LONG_MIN, LONG_MAX);
+    printf("O tipo 'unsigned long int' aceita valores entre %i e %lu.\n", 0, ULONG_MAX);
+    printf("O tipo 'long long int' aceita valores entre %lli e %lli.\n", LLONG_MIN, LLONG_MAX);
+    printf("O tipo 'unsigned long long int' aceita valores entre %i e %lu.\n", 0, ULLONG_MAX);
+
+
+    return 0;
+}
