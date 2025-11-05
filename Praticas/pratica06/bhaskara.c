cccaff6af7601ccd81e9cb84c8df5930a06f2972
Gustavo <gustiftw021@gmail.com>
2025-11-05 10:44:47 -0300

    adicionando arquivos da aula08

--- Praticas/pratica06/bhaskara.c
+++ Praticas/pratica06/bhaskara.c
@@ -3,28 +3,30 @@
 
 int main () {
 
-    int a;
-    int b;
-    int c;
+    double a, b, c;
+    double delta, x1, x2;
 
-    printf("Insira o valor de a\n ", a);
-    scanf("%d", &a);
-    printf("Insira o valor de b\n ", b);
-    scanf("%d", &b);
-    printf("Insira o valor de c\n ", c);
-    scanf("%d", &c);
+    printf("Digite os valores de a, b e c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
 
-    float delta = b*b - 4*a*c;
+    printf("Lidos: a = %.2lf, b = %.2lf, c = %.2lf\n", a, b, c);
+
+
+    delta = b*b - 4*a*c;
+    printf("Delta = %.2lf\n", delta);
 
     if (delta < 0) {
-        printf("A equação naum tem raizes reais\n");
+        printf("A equaçao naum tem raizes reais\n");
     } 
-    else (delta > 0); 
-    {
-        float x1 = (-b + sqrt(delta)) / 2*a;
-        float x2 = (-b - sqrt(delta)) / 2*a;
-        printf("o valor da 1 raiz eh: %d\n", x1);
-        printf("o valor da 2 raiz eh: %d\n", x2);
+    else if  (delta == 0) 
+    { x1 = -b / (2 * a);
+    printf("Existe uma raiz real : x = %.2lf\n", x1);
+    } else {
+        float x1 = (-b + sqrt(delta)) / (2 * a);
+        float x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("As duas raizes reais sao:\n");
+        printf("x1 = %.2lf\n", x1);
+        printf("x2 = %.2lf\n", x2);
     } 
 
 
